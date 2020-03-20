d796ac9bb08d5df7b5b8575d3045b0d2d6be27db
Matt R <mattrdk2@gmail.com>
2020-03-19 20:35:18 -0700

    Update shootout.py

--- shootout.py
+++ shootout.py
@@ -7,7 +7,6 @@ import time
 
 clear = lambda: os.system('cls')
 
-# Current selection of teams, more to come!!!
 real = [("Banzema", 83), ("Vini Jr", 82), ("Modric", 84), ("Ramos", 86), ('Lucas', 80), ("Navas", 72)]
 
 barca = [("Messi", 91), ("Griezmann", 85), ("Dembele", 83), ("Suarez", 85), ("De Jong", 74), ("Ter Stegen", 75)]
